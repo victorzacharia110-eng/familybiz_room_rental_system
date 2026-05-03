a84476fd28e0d2a864af32febe284b8f4a2dfac5
Victor Zacharia <victorzacharia110@email.com>
2026-05-03 08:29:30 +0300

    Fix production API URL

--- src/composables/api.js
+++ src/composables/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://parted-amusement-ripping.ngrok-free.dev',
+  baseURL: 'https://api.familybiz.online',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
