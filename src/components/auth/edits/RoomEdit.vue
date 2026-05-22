971328c1e81ff8646195bdfff85f6d81691f58db
Victor Zacharia <victorzacharia110@email.com>
2026-05-22 17:56:17 +0300

    ref was missing in RoomEdit.vue

--- src/components/auth/edits/RoomEdit.vue
+++ src/components/auth/edits/RoomEdit.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useRoomStore } from '@/stores/room'
 import { useI18n } from 'vue-i18n'
