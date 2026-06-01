c6e9df45f7aacff5fa06514be6ccce33aaaad609
Victor Zacharia <victorzacharia110@email.com>
2026-06-01 15:24:16 +0300

    increased the modal  wrap width

--- src/components/auth/LandLordDashboard.vue
+++ src/components/auth/LandLordDashboard.vue
@@ -2343,6 +2343,7 @@ tbody tr:last-child td {
   border: 1px solid rgba(255, 255, 255, 0.07);
   overflow: hidden;
   max-height: 280px;
+  max-width: 600px;
   overflow-y: auto;
   overflow-x: auto;
 }
