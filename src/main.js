adac00fa92cd85e18888df6060b9bed665442d69
Victor Zacharia <victorzacharia110@email.com>
2026-06-08 12:30:36 +0300

    updated the component files  to remove emojis with real icons

--- src/main.js
+++ src/main.js
@@ -5,17 +5,97 @@ import router from './router'
 import i18n from './i18n'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './assets/base.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+// Import ALL icons
+import {
+  faChartBar,
+  faBullhorn,
+  faComments,
+  faUser,
+  faLock,
+  faDoorOpen,
+  faMoneyBill,
+  faCreditCard,
+  faCheckCircle,
+  faBuilding,
+  faHouse,
+  faBars,
+  faBan,
+  faStar,
+  faMobileAlt,
+  faRightFromBracket,
+  faTrash,
+  faXmark,
+  faSpinner,
+  faEnvelope,
+  faExclamationTriangle,
+  faInfoCircle,
+  faCalendarAlt,
+  faImage,
+  faKey,
+  faSave,
+  faArrowLeft,
+  faHashtag,
+  faHome,
+  faChartSimple,
+  faSatelliteDish,
+  faHeart,
+  faCircle,
+  faLandmark,
+  faPhone,
+  faEye,
+  faEyeSlash,
+  faTimesCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
+// Add all icons to library
+library.add(
+  faChartBar,
+  faBullhorn,
+  faComments,
+  faUser,
+  faLock,
+  faDoorOpen,
+  faMoneyBill,
+  faCreditCard,
+  faCheckCircle,
+  faBuilding,
+  faHouse,
+  faBars,
+  faBan,
+  faStar,
+  faMobileAlt,
+  faRightFromBracket,
+  faTrash,
+  faXmark,
+  faSpinner,
+  faEnvelope,
+  faExclamationTriangle,
+  faInfoCircle,
+  faCalendarAlt,
+  faImage,
+  faKey,
+  faSave,
+  faArrowLeft,
+  faHashtag,
+  faHome,
+  faChartSimple,
+  faSatelliteDish,
+  faHeart,
+  faCircle,
+  faLandmark,
+  faPhone,
+  faEye,
+  faEyeSlash,
+  faTimesCircle,
+)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
-// creating pinia instance
 const pinia = createPinia()
-
-// register the persistence plugin
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
