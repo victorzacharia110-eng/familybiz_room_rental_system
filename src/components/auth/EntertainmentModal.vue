f576e4fc2b0051652f40ae8ecda2591db9a3898f
Victor Zacharia <victorzacharia110@email.com>
2026-06-17 17:27:10 +0300

    added entertainment functionality to the system v1.2

--- src/components/auth/EntertainmentModal.vue
+++ src/components/auth/EntertainmentModal.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue'
-import { useFootballStore } from '@/stores/football'
+import { useFootballStore } from '@/stores/entertainment/'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const props = defineProps({
